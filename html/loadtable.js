5b256e5e23909827dbacfd4a1a4da066746cadb0
Robin M <robinmacmillan0@gmail.com>
2021-07-16 18:20:16 -0700

    Updated table to point to correct json

--- html/loadtable.js
+++ html/loadtable.js
@@ -1,5 +1,5 @@
 function loadTable() {
-$.getJSON('PROFIT.json', function(data){
+$.getJSON('../PROFIT.json', function(data){
     $.each(data, function(key, value){
     marketorder = '<tr>' + '<td>' + value.item + '</td>' + '<td>' + value.from + '</td>' + '<td>' + value.to + '</td>' + '<td>' + value.quantity + '</td>' + '<td>' + value.profit + '</td>'  + '</tr>';
     $('#marketorders').append(marketorder);
